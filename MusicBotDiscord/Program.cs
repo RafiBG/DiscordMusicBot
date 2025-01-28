98ac1bc004ac0f4550ed871bbd033a09d70ec1ce
BlueDiamond (Rafi) <32616741+RafiBG@users.noreply.github.com>
2025-01-28 21:55:48 +0200

    Update Program.cs

--- MusicBotDiscord/Program.cs
+++ MusicBotDiscord/Program.cs
@@ -18,7 +18,7 @@ public class Program
         var jsonReader = new JSONReader();
         await jsonReader.ReadJSON();
 
-        if (string.IsNullOrWhiteSpace(jsonReader.token));
+        if (string.IsNullOrWhiteSpace(jsonReader.token))
         {
             Console.WriteLine("The token in config.json is missing or invalid. Please provide a valid bot token.");
         }
